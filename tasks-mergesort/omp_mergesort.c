9160d9a918613cca7e960c0f3f9aa546977c6c5e
Ivan Fekete <ivan_fekete@ukr.net>
2020-06-06 17:12:58 +0300

    Update omp_mergesort.c

--- tasks-mergesort/omp_mergesort.c
+++ tasks-mergesort/omp_mergesort.c
@@ -1,3 +1,4 @@
+//gcc-9 -fopenmp omp_mergesort.c -o omp_mergesort
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -6,133 +7,11 @@
 
 extern double get_time(void);
 
-void merge(int a[], int size, int temp[]);
-
-void insertion_sort(int a[], int size);
-
-void mergesort_serial(int a[], int size, int temp[], int thresh);
-
-void mergesort_parallel_omp(int a[], int size, int temp[], int threads, int thresh);
-
-void run_omp(int a[], int size, int temp[], int threads, int thresh);
-
-
-int main(int argc, char *argv[]) {
-    if (argc != 4)
-    {
-        printf("Usage: %s array_size threshold num_threads\n", argv[0]);
-        return 1;
-    }
-
-    int size = atoi(argv[1]);
-	int thresh = atoi(argv[2]);
-    int threads = atoi(argv[3]);
-	
-    // Check nested parallelism availability
-
-    omp_set_nested(1);
-    if (omp_get_nested() != 1) {
-        puts("Warning: Nested parallelism desired but unavailable");
-    }
-
-    // Check processors and threads
-    int processors = omp_get_num_procs();    // Available processors
-
-    if (threads > processors) {
-        omp_set_num_threads(threads);
-    }
-    int max_threads = omp_get_max_threads();    // Max available threads
-    if (threads > max_threads)    // Requested threads are more than max available
-    {
-        printf("Error: Cannot use %d threads, only %d threads available\n",
-               threads, max_threads);
-        return 1;
-    }
-
-    // Array allocation
-    int *a = malloc(sizeof(int) * size);
-    int *temp = malloc(sizeof(int) * size);
-    if (a == NULL || temp == NULL) {
-        printf("Error: Could not allocate array of size %d\n", size);
-        return 1;
-    }
-    // Random array initialization
-    int i;
-    srand(314159);
-    for (i = 0; i < size; i++) {
-        a[i] = rand() % size;
-    }
-
-    // run sort and get time
-    double start = get_time();
-    run_omp(a, size, temp, threads, thresh);
-    double end = get_time();
-    printf("%.4f\n", end - start);
-
-    // check sorted
-    for (i = 1; i < size; i++) {
-        if (!(a[i - 1] <= a[i])) {
-            printf("Error: final array not sorted => a[%d]=%d > a[%d]=%d\n", i - 1,
-                   a[i - 1], i, a[i]);
-            return 1;
-        }
-    }
-    return 0;
-}
-
-
-void run_omp(int a[], int size, int temp[], int threads, int thresh) {
-    omp_set_nested(1);
-    mergesort_parallel_omp(a, size, temp, threads, thresh);
-}
-
-
-void mergesort_parallel_omp(int a[], int size, int temp[], int threads, int thresh) {
-    if (threads == 1) {
-        mergesort_serial(a, size, temp, thresh);
-    }
-    else if (threads > 1) {
-        #pragma omp parallel
-        {
-            #pragma omp single nowait
-            {
-                #pragma omp task
-                {
-                    mergesort_parallel_omp(a, size / 2, temp, threads / 2, thresh);
-                }
-                #pragma omp task
-              	{
-                		mergesort_parallel_omp(a + size / 2, size - size / 2,
-                                           temp + size / 2, threads - threads / 2, thresh);
-                }
-		#pragma omp taskwait
-                {
-                    merge(a, size, temp);
-                }
-            }
-        }
-    }
-    else {
-        printf("Error: %d threads\n", threads);
-        return;
-    }
-}
-
-void mergesort_serial(int a[], int size, int temp[], int thresh) {
-    if (size <= thresh) {
-        insertion_sort(a, size);
-        return;
-    }
-    mergesort_serial(a, size / 2, temp, thresh);
-    mergesort_serial(a + size / 2, size - size / 2, temp, thresh);
-    merge(a, size, temp);
-}
-
-void merge(int a[], int size, int temp[]) {
+void merge(int a[], int size, int size1, int temp[]) {
     int i1 = 0;
-    int i2 = size / 2;
+    int i2 = size1;
     int tempi = 0;
-    while (i1 < size / 2 && i2 < size) {
+    while (i1 < size1 && i2 < size) {
         if (a[i1] < a[i2]) {
             temp[tempi] = a[i1];
             i1++;
@@ -142,7 +21,7 @@ void merge(int a[], int size, int temp[]) {
         }
         tempi++;
     }
-    while (i1 < size / 2) {
+    while (i1 < size1) {
         temp[tempi] = a[i1];
         i1++;
         tempi++;
@@ -155,15 +34,61 @@ void merge(int a[], int size, int temp[]) {
     memcpy(a, temp, size * sizeof(int));
 }
 
-void insertion_sort(int a[], int size) {
-    int i;
-    for (i = 0; i < size; i++) {
-        int j, v = a[i];
-        for (j = i - 1; j >= 0; j--) {
-            if (a[j] <= v)
-                break;
-            a[j + 1] = a[j];
-        }
-        a[j + 1] = v;
+
+void mergesort_serial(int a[], int size, int temp[]) {
+    if (size > 1) {
+        mergesort_serial(a, size / 2, temp);
+        mergesort_serial(a + size / 2, size - size / 2, temp);
+        merge(a, size, size / 2, temp);
     }
 }
+
+int main(int argc, char *argv[]) {
+    int size = atoi(argv[1]);
+    int threads = atoi(argv[2]);
+
+    omp_set_nested(1);
+    omp_set_num_threads(threads);
+
+    int *a = malloc(sizeof(int) * size);
+    int *temp = malloc(sizeof(int) * size);
+
+    int i;
+    srand(314159);
+    for (i = 0; i < size; i++) {
+        a[i] = rand() % size;
+    }
+
+
+    int part_size = size / threads;
+    int first_part = size % part_size + part_size;
+    double start = get_time();
+    {
+#pragma omp parallel for shared(a, temp)
+        for (int i = 0; i < threads; i++) {
+            if (i == 0) {
+                mergesort_serial(a, first_part, temp);
+            } else {
+                mergesort_serial(a + first_part + (i - 1) * part_size, part_size, temp + + first_part + (i - 1) * part_size);
+            }
+        }
+
+        for (int i = 1; i < threads; i++) {
+            merge(a, first_part + i * part_size, first_part + (i - 1) * part_size, temp);
+        }
+    }
+    double end = get_time();
+    printf("%.4f\n", end - start);
+
+
+    for (i = 1; i < size; i++) {
+        if (!(a[i - 1] <= a[i])) {
+            printf("Error: final array not sorted => a[%d]=%d > a[%d]=%d\n", i - 1,
+                   a[i - 1], i, a[i]);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
